2ded28527811d02e77dd3969dd2dbd397550693b
MahfujSajibe <mahfujsajib1998@gmail.comm>
2021-11-13 22:58:19 +0600

    add purchased

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,11 +1,33 @@
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <img className='img-fluid mt-3' src=" https://pro-theme.com/html/autozone/assets/media/main-slider/1.jpg" alt="" />
-        </div>
+
+        <Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4} sx={{ marginTop: 5 }}>
+                    <Link to='/carlists' style={{ textDecoration: 'none' }}>
+                        <Button style={{
+                            backgroundColor: 'black',
+                            marginTop: "200px",
+                            color: '#fff',
+                            '&:hover': {
+                                color: 'black',
+                            },
+                        }}>Explore More</Button>
+                    </Link>
+                </Grid >
+                <Grid item xs={12} sm={6} md={8} sx={{ marginTop: 5 }}>
+                    <img className='img-fluid mt-3' src=" https://pro-theme.com/html/autozone/assets/media/main-slider/1.jpg" alt="" />
+                </Grid >
+            </Grid>
+        </Box>
+
     );
 };
 
